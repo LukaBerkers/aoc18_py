06dce1a00421881e76ce7b00658993cf2818eedb
Luka <luka@berkers.dev>
2024-09-21 17:00:30 +0200

    feat: add logging and error handling for file operations
    Introduced logging setup and critical error handling in `main.py` for better traceability. Updated `day_1.py` to include logging and detailed docstrings, along with enhanced error handling when reading the frequency changes file.

--- day_1.py
+++ day_1.py
@@ -1,19 +1,42 @@
+import logging
 from pathlib import Path
 
 from utils import INPUT_DIR
 
-_FREQUENCY_CHANGES_FILE_NAME: Path = INPUT_DIR / "day_1.txt"
+logger = logging.getLogger(__name__)
+
+_FREQUENCY_CHANGES_FILE_NAME: Path = INPUT_DIR / 'day_1.txt'
 
 
 def read_frequency_changes() -> list[int]:
+    """
+    Reads frequency changes from the input file and returns them as a list of integers.
+
+    Raises:
+        OSError: If the file cannot be opened or read.
+
+    Returns:
+        list[int]: A list of integer frequency changes.
+    """
     frequency_changes = list[int]()
 
-    with _FREQUENCY_CHANGES_FILE_NAME.open() as frequency_changes_file:
-        for line in frequency_changes_file:
-            frequency_changes.append(int(line))
+    try:
+        with _FREQUENCY_CHANGES_FILE_NAME.open() as frequency_changes_file:
+            for line in frequency_changes_file:
+                frequency_changes.append(int(line))
+    except OSError as error:
+        logger.error(error)
+        raise
 
     return frequency_changes
 
 
 def apply_frequency_changes(frequency_changes: list[int]) -> int:
+    """
+    Args:
+        frequency_changes (list[int]): A list of integer frequency changes.
+
+    Returns:
+        int: The resulting frequency after applying all the changes.
+    """
     return sum(frequency_changes)
--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+import logging
+
 import day_1
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d - %(message)s')
+logger = logging.getLogger(__name__)
 
 
 def main():
-    frequency_changes = day_1.read_frequency_changes()
+    try:
+        frequency_changes = day_1.read_frequency_changes()
+    except OSError:
+        logger.critical('Could not read the file of frequency changes.')
+        return
+
     final_frequency = day_1.apply_frequency_changes(frequency_changes)
     print(final_frequency)
 
