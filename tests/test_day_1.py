d0796d0914b03800605e9bf85cbe1f171c769c0f
Luka <luka@berkers.dev>
2024-09-27 20:31:55 +0200

    test: add unit tests for find_first_repeated_frequency
    Introduced three unit tests to ensure proper functionality of find_first_repeated_frequency. Tests cover edge cases such as recursion limit, looping behavior, and scenarios with/without frequency changes.

---
+++ tests/test_day_1.py
@@ -0,0 +1,82 @@
+# This file is part of my solutions for Advent of Code 2018.
+# Copyright (C) 2024  Luka Berkers
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import unittest
+from typing import Optional
+
+from hypothesis import assume, given, strategies as st
+
+import day_1
+
+
+class TestFindFirstRepeatedFrequency(unittest.TestCase):
+    # pp 6, 7, 8, 9
+    @given(st.integers(), st.one_of(st.sets(st.integers(), max_size=0), st.none()))
+    def test_without_frequencies_encountered_no_frequency_changes_reaches_recursion_limit(
+            self, current_frequency: int, frequencies_encountered: Optional[set[int]]
+    ):
+        # Arrange
+        frequency_changes = []
+
+        # Act, Assert
+        with self.assertRaises(RecursionError):
+            day_1.find_first_repeated_frequency(
+                frequency_changes, current_frequency, frequencies_encountered
+            )
+
+    # pp 10, 11, 13
+    @given(st.lists(st.integers(), min_size=3), st.integers(), st.sets(st.integers()))
+    def test_loops_when_frequency_is_not_yet_encountered(
+            self,
+            frequency_changes: list[int],
+            current_frequency: int,
+            frequencies_encountered: set[int]
+    ):
+        # Arrange
+        after_first_frequency_change = current_frequency + frequency_changes[0]
+        after_second_frequency_change = after_first_frequency_change + frequency_changes[1]
+        after_third_frequency_change = after_second_frequency_change + frequency_changes[2]
+        assume(after_first_frequency_change != after_second_frequency_change)
+        assume(after_second_frequency_change != after_third_frequency_change)
+        frequencies_encountered.discard(after_first_frequency_change)
+        frequencies_encountered.discard(after_second_frequency_change)
+        frequencies_encountered.add(after_third_frequency_change)
+
+        # Act
+        result = day_1.find_first_repeated_frequency(
+            frequency_changes, current_frequency, frequencies_encountered
+        )
+
+        # Assert
+        self.assertEqual(after_third_frequency_change, result)
+
+    # pp 12, 14, 15
+    @given(st.integers(), st.sets(st.integers(), min_size=1))
+    def test_with_frequencies_encountered_no_frequency_changes_reaches_recursion_limit(
+            self, current_frequency: int, frequencies_encountered: set[int]
+    ):
+        # Arrange
+        frequency_changes = []
+
+        # Act, Assert
+        with self.assertRaises(RecursionError):
+            day_1.find_first_repeated_frequency(
+                frequency_changes, current_frequency, frequencies_encountered
+            )
+
+
+if __name__ == '__main__':
+    unittest.main()
